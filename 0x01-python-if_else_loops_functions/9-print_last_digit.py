486e7c5f4e333ca689d080ccbf0b2277c8c4d0b6
girumtim <girumtim@gmail.com>
2022-06-08 00:25:51 -0700

    task-9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    i = number % 10
-    if (i < 0):
-        i = i * -1
-    print(i, end="")
+    i = abs(number) % 10
+    print("{:d}".format(i), end="")
     return (i)
